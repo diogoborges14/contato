c3277b7bbdc5628870dd6a157d82a3b45a8b6f1e
Diogo B013 <diogob013@outlook.com>
2022-04-09 18:31:47 -0300

    connected to the new linked list

--- contatos.c
+++ contatos.c
@@ -10,8 +10,10 @@
 */
 
 #include <gtk/gtk.h>
-#include "include/dynamicList.h"
+#include "include/linkedContactList.h"
 #include <stdlib.h>
+
+CONTACT_LIST* contactListDB;
 
 /*****   Widgets   *****/
 GtkBuilder         *builder;
@@ -19,16 +21,13 @@ GtkWindow          *mainWindow;
 GtkStack           *viewStack;
 GtkTreeView        *treeView;
 GtkTreeModelFilter *filteredList;
-GtkListStore       *contactList;
+GtkListStore       *contactsModel;
 GtkMessageDialog   *msgBox;
 GtkSearchEntry     *inpSearch;
 GtkEntry           *inpName;
 GtkEntry           *inpPhoneNumber;
 GtkEntry           *inpEmail;
 
-int id = 0;
-USER *headUser; // Where my list start
-USER *auxiliarUser;
 
 void showMsgBox(char text[100], char secondary_text[100], char icon_name[100])
 {
@@ -87,72 +86,56 @@ void on_btnRemove_clicked()
         gtk_tree_model_get(model, &iter,
                            0, &selectedId, // get from column 0
                            -1);
-        USER *aux = headUser;
-        USER *prev = headUser;
-        while( (aux->next != NULL) && (aux->id != selectedId) ){ // Search on linked list
-            prev = aux;
-            aux = aux->next;
-        }
-        if(aux->id == selectedId) // Verify search results
-            prev->next = aux->next;
-        if(aux != NULL)
-            free(aux);
+        contact_list_remove(contactListDB, selectedId); // Remove from storage
 
-        gtk_list_store_remove(contactList, &iter); // Remove from view
+        gtk_list_store_remove(contactsModel, &iter); // Remove from view
     }
 }
 
 void on_btnReloadList_clicked()
 {
-    auxiliarUser->next = NULL; // initialize var
-    auxiliarUser = headUser; // Get where list start
-
+    PERSON *tmpPerson;
     GtkTreeIter iter;
-    gtk_list_store_clear(contactList);
+    gtk_list_store_clear(contactsModel);
 
     // Populate list_store with data from dynamic list
-    while(auxiliarUser->next != NULL){
-        gtk_list_store_append(contactList, &iter);
-        gtk_list_store_set(contactList, &iter,
-                           0, auxiliarUser->id,
-                           1, auxiliarUser->nome,
-                           2, auxiliarUser->telefone,
-                           3, auxiliarUser->email,
-                           -1);
+    for(int i=1; i <= contact_list_get_length(contactListDB); i++){
+        tmpPerson = contact_list_get_person_by_position(contactListDB, i);
 
-        auxiliarUser = auxiliarUser->next;
+        // Copy to TreeView model
+        //if(tmpPerson){
+            gtk_list_store_append(contactsModel, &iter);
+            gtk_list_store_set(contactsModel, &iter,
+                            0, tmpPerson->id,
+                            1, tmpPerson->name,
+                            2, tmpPerson->phone,
+                            3, tmpPerson->email,
+                            -1);
+        //}
     }
-
-    // Sort list in ascending order by column 1 (name)
-    GtkTreeSortable *sortableDataStore = GTK_TREE_SORTABLE(contactList);
-    gtk_tree_sortable_set_sort_column_id(sortableDataStore, 1, GTK_SORT_ASCENDING);
 }
 
 void on_btnAddRegister_clicked()
 {
     char txt[50] = "\0";
-    const char *nome     = gtk_entry_get_text(inpName);
-    const char *telefone = gtk_entry_get_text(inpPhoneNumber);
+    PERSON person;
+    const char *name     = gtk_entry_get_text(inpName);
+    const char *phoneNum = gtk_entry_get_text(inpPhoneNumber);
     const char *email    = gtk_entry_get_text(inpEmail);
 
-    if(g_str_equal(nome, "")){
+    if(g_str_equal(name, "")){
         showMsgBox("Atenção!", "Nome é obrigatório", "dialog-error");
     }else if(g_str_equal(email, "")){
         showMsgBox("Atenção!", "Email é obrigatório", "dialog-error");
     }else{
-        /* save the user */
-        id++;
-        auxiliarUser->id = id;
-        g_strlcpy(auxiliarUser->nome,     nome,    100);
-        g_strlcpy(auxiliarUser->telefone, telefone, 15);
-        g_strlcpy(auxiliarUser->email,    email,   100);
+        /* save the person */
+        g_strlcpy(person.name,     name,    100);
+        g_strlcpy(person.phone,    phoneNum, 15);
+        g_strlcpy(person.email,    email,   100);
+        contact_list_add(contactListDB, person);
 
-        g_snprintf(txt, 50, "Usuário %s cadastrado com sucesso.", nome);
+        g_snprintf(txt, 50, "Contato %s salvo com sucesso.", name);
         showMsgBox("Aviso", txt, "dialog-emblem-default");
-
-        /* alloc space for next user */
-        auxiliarUser->next = (USER*)malloc(sizeof(USER));
-        auxiliarUser = auxiliarUser->next; // auxiliarUser become the newlest allocated new_next user
     }
 }
 
@@ -165,8 +148,7 @@ void on_btnGoBack_clicked()
 
 void startApplication(GtkApplication *app, gpointer user_data)
 {
-    headUser = (USER*)malloc(sizeof(USER));
-    auxiliarUser = headUser;
+    contactListDB = contact_list_new_from_file();
 
     // Getting widgets from view
     builder          = gtk_builder_new_from_file("include/contatos.ui");
@@ -174,7 +156,7 @@ void startApplication(GtkApplication *app, gpointer user_data)
     treeView         = GTK_TREE_VIEW(gtk_builder_get_object(builder, "treeView"));
     inpSearch        = GTK_SEARCH_ENTRY(gtk_builder_get_object(builder, "inpSearch"));
     viewStack        = GTK_STACK(gtk_builder_get_object(builder, "viewStack"));
-    contactList      = GTK_LIST_STORE(gtk_builder_get_object(builder, "contactList"));
+    contactsModel    = GTK_LIST_STORE(gtk_builder_get_object(builder, "contactList"));
     msgBox           = GTK_MESSAGE_DIALOG(gtk_builder_get_object(builder, "msgBox"));
     inpName          = GTK_ENTRY(gtk_builder_get_object(builder, "inpName"));
     inpPhoneNumber   = GTK_ENTRY(gtk_builder_get_object(builder, "inpPhoneNumber"));
